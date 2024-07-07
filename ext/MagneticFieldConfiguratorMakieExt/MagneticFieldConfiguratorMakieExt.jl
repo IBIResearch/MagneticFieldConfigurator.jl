d7ed7ec27c62debafc7f1e4c6699b115904aab8c
Tobias Knopp <tobias.knopp@tuhh.de>
2024-07-07 10:46:23 +0200

    improve magnetic field viewer

--- ext/MagneticFieldConfiguratorMakieExt/MagneticFieldConfiguratorMakieExt.jl
+++ ext/MagneticFieldConfiguratorMakieExt/MagneticFieldConfiguratorMakieExt.jl
@@ -2,16 +2,22 @@ module MagneticFieldConfiguratorMakieExt
 
 using MagneticFieldConfigurator, Makie, LinearAlgebra
 
-const colors = [RGBf.(0/255,73/255,146/255), # blue
-  RGBf.(239/255,123/255,5/255),	# orange (dark)
-  RGBf.(138/255,189/255,36/255),	# green
-  RGBf.(178/255,34/255,41/255), # red
-  RGBf.(170/255,156/255,143/255), # mocca
-  RGBf.(87/255,87/255,86/255),	# black (text)
-  RGBf.(255/255,223/255,0/255), # yellow
-  RGBf.(104/255,195/255,205/255),# "TUHH"
-  RGBf.(45/255,198/255,214/255), #  TUHH
-  RGBf.(193/255,216/255,237/255)]
+export ibicolors
+
+function ibicolors(i::Integer)
+  ibicolors_ = [RGBf.(0/255,73/255,146/255), # blue
+	  RGBf.(239/255,123/255,5/255),	# orange (dark)
+	  RGBf.(138/255,189/255,36/255),	# green
+	  RGBf.(178/255,34/255,41/255), # red
+	  RGBf.(170/255,156/255,143/255), # mocca
+	  RGBf.(87/255,87/255,86/255),	# black (text)
+	  RGBf.(255/255,223/255,0/255), # yellow
+	  RGBf.(104/255,195/255,205/255),# "TUHH"
+	  RGBf.(45/255,198/255,214/255), #  TUHH
+	  RGBf.(193/255,216/255,237/255)]
+
+  return ibicolors_[mod1(i,length(ibicolors_))]
+end
 
 function Makie.plot!(ax, cc::ComposedField, shift=[0,0,0], scale=1)
 
@@ -47,7 +53,7 @@ function Makie.plot!(ax, c::CoordinateSystem; scale=1)
 
   pl = arrows!(ax, [centerP,centerP,centerP], [ Point3f(c.basis[:,1]...)*factor, 
      Point3f(c.basis[:,2]...)*factor, Point3f(c.basis[:,3]...)*2*factor], 
-    linewidth=factor/8, color=[colors[2],colors[1],:black], arrowsize=factor/4)
+    linewidth=factor/8, color=[ibicolors(2),ibicolors(1),:black], arrowsize=factor/4)
 
   return pl
 end
--- ext/MagneticFieldConfiguratorMakieExt/MagneticFieldViewer.jl
+++ ext/MagneticFieldConfiguratorMakieExt/MagneticFieldViewer.jl
@@ -1,8 +1,154 @@
 
 
 function MagneticFieldConfigurator.viewer(system::MagneticFieldSystem)
-  fig = Figure()
+
+  fig = Figure(size=(900,700), figure_padding = 10, backgroundcolor = :gray97)
+
   ax = Axis3(fig[1,1], xlabel = "x / cm", ylabel = "y / cm", zlabel = "z / cm")
+  cb = Colorbar(fig[1, 2], limits=(0.0,1.0), label="H / mT")
+
+  grid = GridLayout(fig[2, 1:2], tellwidth=false, alignmode = Outside(0,0,0,10))
+
+  plot!(ax, system.generators)
+  α = 0.1
+  colorsbg = [ c*α + (1-α)*colorant"white" for c in 
+              [ibicolors(1),ibicolors(2),ibicolors(3),ibicolors(4)] ]
+  α = 0.3
+  colorsfg = [ c*α + (1-α)*colorant"white" for c in 
+               [ibicolors(1),ibicolors(2),ibicolors(3),ibicolors(4)] ]
+
+  box1 = Box(grid[1,1], cornerradius = 10, color = colorsbg[1], strokecolor = colorsfg[1], strokewidth = 2)
+  box2 = Box(grid[1,2], cornerradius = 10, color = colorsbg[2], strokecolor = colorsfg[2], strokewidth = 2)
+  box3 = Box(grid[1,3], cornerradius = 10, color = colorsbg[3], strokecolor = colorsfg[3], strokewidth = 2) 
+
+  tbGridSize = Textbox(fig, stored_string = "11 x 11 x 11", validator = r"\s*\d+\s*x\s*\d+\s*x\s*\d+\s*")
+  tbGridFov = Textbox(fig, stored_string = "0.1 x 0.1 x 0.1", validator = r"\s*\d+(\.\d+)?\s*x\s*\d+(\.\d+)?\s*x\s*\d+(\.\d+)?\s*" )
+  tbGridCenter = Textbox(fig, stored_string = "0.0 x 0.0 x 0.0", validator = r"\s*\d+(\.\d+)?\s*x\s*\d+(\.\d+)?\s*x\s*\d+(\.\d+)?\s*" )
+
+  btnShowSliceX = Toggle(fig, active=true)
+  btnShowSliceY = Toggle(fig, active=true)
+  btnShowSliceZ = Toggle(fig, active=true)
+
+
+
+  grid1 = grid!([1, 1] => Label(fig, "Grid Size:"), [1, 2] => tbGridSize,
+                [2, 1] => Label(fig, "Grid Fov:"), [2, 2] => tbGridFov,
+                [3, 1] => Label(fig, "Grid Center:"), [3, 2] => tbGridCenter,
+                [1, 3] => Label(fig, "Slice X:"), [1, 4] => btnShowSliceX,
+                [2, 3] => Label(fig, "Slice Y:"), [2, 4] => btnShowSliceY,
+                [3, 3] => Label(fig, "Slice Z:"), [3, 4] => btnShowSliceZ,
+                alignmode = Outside(5))
+                grid[1,1] = grid1 
+  sg = SliderGrid(
+    grid1[1:3, 5],
+    (label = "x", range = 1:11,  startvalue = 5),
+    (label = "y", range = 1:11,  startvalue = 5),
+    (label = "z", range = 1:11,  startvalue = 5),
+    width = 350,
+    tellheight = false)
+
+  on(tbGridSize.stored_string) do s
+    gridsize = parse.(Int, split(tbGridSize.stored_string[], "x"))
+    for d=1:3
+      sg.sliders[d].range[] = 1:gridsize[d]
+    end
+  end
+
+                  
+  rowgap!(grid1, 10); colgap!(grid1, 10)
+
+  tbCurrents = Textbox(fig, stored_string = join(system.source.currents, ","), 
+                        validator = r"^\s*-?\d+(\.\d+)?(\s*,\s*-?\d+(\.\d+)?)*\s*$" )
+
+
+  grid2 = grid!([1, 1] =>  Label(fig,"Currents"), [1, 2] => tbCurrents, 
+                alignmode = Outside(5))
+  grid[1,2] = grid2
+  rowgap!(grid2, 10); colgap!(grid2, 10)
+  colgap!(grid, 10)
+
+  btnCalc = Button(fig, label = "Calculate Field")
+  grid3 = grid!([1, 1] => btnCalc, 
+                alignmode = Outside(5))
+  grid[1,3] = grid3
+  rowgap!(grid3, 10); colgap!(grid3, 10)
+  colgap!(grid, 10)
+
+
+  rowgap!(fig.layout, Relative(0.01))
+
+
+  on(btnCalc.clicks) do n
+    gridsize = parse.(Int, split(tbGridSize.stored_string[], "x"))
+    gridfov = parse.(Float64, split(tbGridFov.stored_string[], "x"))
+    gridcenter = parse.(Float64, split(tbGridCenter.stored_string[], "x"))
+    Δ = gridfov ./ (gridsize .- 1)
+    minFov = gridcenter .- gridfov./2 .+ Δ./2
+    maxFov = gridcenter .+ gridfov./2 .- Δ./2
+
+    empty!(ax)
+    plot!(ax, system.generators)
+
+    currents = parse.(Float64, split(tbCurrents.stored_string[], ","))
+    if length(currents) == length(system.source.currents)
+      system.source.currents .= currents
+    else
+      println("Number of currents does not match number of coils")
+    end
+    
+
+    current!(system.source, system.source.currents)
+
+    z_ = range(minFov[3],maxFov[3],length=gridsize[3])
+    y_ = range(minFov[1],maxFov[1],length=gridsize[1])
+    x_ = range(minFov[1],maxFov[1],length=gridsize[1])
+
+    maxfield = 0.0
+    
+    if btnShowSliceX.active[]
+      fieldXZ = [norm(system.generators[x,0.0,z]) for z in z_, x in x_]
+      maxfield = max(maximum(fieldXZ), maxfield)
+    end
+
+    if btnShowSliceY.active[]
+      fieldYZ = [norm(system.generators[0.0,y,z]) for z in z_, y in y_]
+      maxfield = max(maximum(fieldYZ), maxfield)
+    end
+
+    if btnShowSliceZ.active[]
+      fieldXY = [norm(system.generators[x,y,0.0]) for y in y_, x in x_]
+      maxfield = max(maximum(fieldXY), maxfield)
+    end
+
+    if btnShowSliceX.active[]
+      plXZ = heatmap!(ax, x_, z_, fieldXZ'; transformation=(:xz, y_[sg.sliders[2].value[]]),  
+        colorrange = (0,maxfield) )
+    end
+    
+    if btnShowSliceY.active[]
+      plYZ = heatmap!(ax, y_, z_, fieldYZ'; transformation=(:yz, x_[sg.sliders[1].value[]]),  
+        colorrange = (0,maxfield) )
+    end
+
+    if btnShowSliceZ.active[]
+      plXY = heatmap!(ax, x_, y_, fieldXY'; transformation=(:xy, z_[sg.sliders[3].value[]]),  
+        colorrange = (0,maxfield) )
+    end
+    
+    #empty!(fig[1, 2])
+    cb.limits = (0, maxfield)
+  end
+
+  if isdefined(Main, :GLMakie)
+    display_fig_in_new_window(fig; title="Magnetic Field Viewer")
+  else
+    display(fig)
+  end
+  return fig
+  #return fig, ax
+end
+
+function plotFields(fig, ax, system::MagneticFieldSystem)
 
   x_ = range(-0.15,0.15,length=25)
   y_ = range(-0.15,0.15,length=20)
@@ -25,16 +171,6 @@ function MagneticFieldConfigurator.viewer(system::MagneticFieldSystem)
   arrows!(ax, x_[1:2:end], y_[1:2:end], field[1,1:2:end,1:2:end]'./maximum(absfield), 
           field[2,1:2:end,1:2:end]'./maximum(absfield), 
           arrowsize = 10.5, lengthscale = 0.3, color=:white, linewidth=5)
-
-  plot!(ax, system.generators)
-
-  if isdefined(Main, :GLMakie)
-    display_fig_in_new_window(fig; title="Magnetic Field Viewer")
-  else
-    display(fig)
-  end
-  return fig
-  #return fig, ax
 end
 
 
